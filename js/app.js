7f222d2d6f7dd4b70d910f8535c1dcf9afc04d6c
Mike Seaby <mike@pixelspring.co.uk>
2015-03-28 18:10:14 +0000

    Grand overhaul of nav nearly there..

--- js/app.js
+++ js/app.js
@@ -13,51 +13,24 @@
 // (1)  Nav Menu:
 // ------------------------------------------------
 
-var ww = document.body.clientWidth;
-
 $(document).ready(function() {
-    $(".nav li a").each(function() {
-        if ($(this).next().length > 0) {
-            $(this).addClass("parent");
-        };
-    })
-    $(".toggleMenu").click(function(e) {
+
+  $('body').addClass('js');
+      var $menu = $('#top_menu'),
+          $menulink = $('.menu-link'),
+          $menuTrigger = $('.has-subnav > a');
+
+    $menulink.click(function(e) {
         e.preventDefault();
-        $(this).toggleClass("active");
-        $(".nav").toggle();
+        $menulink.toggleClass('active');
+        $menu.toggleClass('active');
     });
-    adjustMenu();
-})
 
-$(window).bind('resize orientationchange', function() {
-    ww = document.body.clientWidth;
-    adjustMenu();
+    $menuTrigger.click(function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        $this.toggleClass('active').next('ul').toggleClass('active');
+    });
+
+
 });
-
-var adjustMenu = function() {
-    if (ww < 768) {
-        $(".toggleMenu").css("display", "inline-block");
-        if (!$(".toggleMenu").hasClass("active")) {
-            $(".nav").hide();
-        } else {
-            $(".nav").show();
-        }
-        $(".nav li").unbind('mouseenter mouseleave');
-        $(".nav li a.parent").unbind('click').bind('click', function(e) {
-            // must be attached to anchor element to prevent bubbling
-            e.preventDefault();
-            $(this).parent("li").toggleClass("hover");
-        });
-    }
-    else if (ww >= 768) {
-        $(".toggleMenu").css("display", "none");
-        $(".nav").show();
-        $(".nav li").removeClass("hover");
-        $(".nav li a").unbind('click');
-        $(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
-            // must be attached to li so that mouseleave is not triggered when hover over submenu
-            $(this).toggleClass('hover');
-        });
-    }
-}
-
