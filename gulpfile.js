66236a2f8b51eb40af1ace868ce31d03ed188352
Mike Seaby <mike@pixelspring.co.uk>
2016-04-12 15:59:54 +0100

    Update gulp to display pre/post minified css size

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,35 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var cssnano = require('gulp-cssnano');
+var filesize = require('gulp-size');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
 
 gulp.task('workflow', function () {
+
     gulp.src('./sass/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
+        .pipe(filesize({
+            showFiles: true,
+            title: 'Pre Min: '
+        }))
         .pipe(cssnano({
             autoprefixer: false,
             zindex: false
         }))
 
+        .pipe(filesize({
+            showFiles: true,
+            title: 'CssNano: '
+        }))
+
     .pipe(gulp.dest('./css/'))
+
 });
 
 gulp.task('javascript', function() {
