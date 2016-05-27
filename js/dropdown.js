7ef6b6129eca76728d25628f7ecaeac12d399b8e
Mike Seaby <mike@pixelspring.co.uk>
2016-05-27 15:19:03 +0100

    refactor dropdown js into a more modular approach

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,10 +2,10 @@
 // DROPDOWN.JS
 // ------------------------------------------------
 //
-// (1) Get a list of all elements that are dropdown triggers
+// (1) Get a list of all elements that are dropdown triggers: [data-toggle=dropdown] a
 // (2) Add click + blur event listeners
-// (3) Onclick add "open" to the target element's class names / remove if exists
-// (4) Onblur remove "open" from the target element's class names
+// (3) Onclick add "active" to the target element's class names / remove if exists
+// (4) Onblur remove "active" from the target element's class names
 //
 // ------------------------------------------------
 
@@ -26,33 +26,31 @@
  * </div>
  */
 
-// Show a dropdown menu
-function openDropdown(event) {
-    event = event || window.event;
-    var eventTarget = event.currentTarget || event.srcElement;
-    eventTarget.parentElement.classList.toggle('open');
-    return false;
-}
-
-// Close a dropdown menu
-function closeDropdown(event) {
-    event = event || window.event;
-    var eventTarget = event.currentTarget || event.srcElement;
-    eventTarget.parentElement.classList.remove('open');
-
-    // Trigger the click event on the target if it not opening another menu
-    if(event.relatedTarget && event.relatedTarget.getAttribute('data-toggle') !== 'dropdown') {
-        event.relatedTarget.click();
-    }
-    return false;
-}
 
 // Set event listeners for dropdown menus
-var dropdownList = document.querySelectorAll('[data-toggle=dropdown]');
+var dropdownTrigger = document.querySelectorAll("[data-toggle=dropdown] a");
 
-for (var ddlc = 0, dropdown, lenk = dropdownList.length; ddlc < lenk; ddlc++) {
-    dropdown = dropdownList[ddlc];
-    dropdown.setAttribute('tabindex', '0'); // Make onblur work in Chrome
-    dropdown.onclick = openDropdown;
-    dropdown.onblur = closeDropdown;
+for (var x = 0; x < dropdownTrigger.length; x++) {
+
+    // Make onblur work in Chrome / Safari
+    dropdownTrigger[x].setAttribute('tabindex', '0');
+
+    // Toggle dropdowns onclick
+    dropdownTrigger[x].onclick = function() {
+        var subMenu = this.parentNode.getElementsByClassName("nav-dropdown")[0];
+        if (subMenu.classList.contains('active')) {
+            subMenu.classList.remove("active");
+        } else {
+            subMenu.classList.add("active");
+        }
+    }
+
+    // Close dropdowns onblur
+    dropdownTrigger[x].onblur = function() {
+        var subMenu = this.parentNode.getElementsByClassName("nav-dropdown")[0];
+        if (subMenu.classList.contains('active')) {
+            subMenu.classList.remove("active");
+        }
+    }
+
 }
