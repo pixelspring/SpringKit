4b9cc62be2d8b822d23643b3d679ed6001b30210
Mike Seaby <mike@pixelspring.co.uk>
2016-05-31 17:49:45 +0100

    Add offcanvas menu

---
+++ js/offcanvas.js
@@ -0,0 +1,18 @@
+// ---------------------------------------------------
+// Offcanvas Hide and Show:
+// ---------------------------------------------------
+
+"use strict";
+
+var offCanvasToggle = document.querySelectorAll("[data-toggle=offcanvas]");
+
+for (var x = 0; x < offCanvasToggle.length; x++) {
+
+    offCanvasToggle[x].onclick = function() {
+        document.getElementById('offCanvas').classList.toggle('active');
+        container.classList.toggle('container-pushed');
+        offCanvasMask.classList.toggle('active');
+        document.body.classList.toggle('body-pushed');
+    }
+
+}
